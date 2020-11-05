91bf02fea18034ad496df366a5a8e74278fd6606
Sophie <sophiedarrouxmartin@gmail.com>
2020-11-05 12:47:35 +0000

    Creating a combine test case with mark.parameterize Answers: UCL-RITS/rse-classwork-2020/issues/89

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,6 +1,28 @@
 from times import compute_overlap_time, time_range
+import pytest 
 from pytest import raises
 
+large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+
+# Defining more input ranges
+differentday = time_range("2010-01-13 10:30:00", "2010-01-13 10:45:00", 2, 60)
+large_3_intervals = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 3, 60)
+short_3_intervals = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 3, 60)
+short_boundary_overlap = time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00")
+
+# Defining expected
+empty = []
+large_short = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+large_3_intervals_short_3_intervals = [('2010-01-12 10:30:00', '2010-01-12 10:34:20'), ('2010-01-12 10:35:20', '2010-01-12 10:39:20'), ('2010-01-12 10:40:20', '2010-01-12 10:34:20'), ('2010-01-12 10:40:20', '2010-01-12 10:39:40'), ('2010-01-12 10:40:40', '2010-01-12 10:45:00'), ('2010-01-12 11:20:40', '2010-01-12 10:34:20'), ('2010-01-12 11:20:40', '2010-01-12 10:39:40'), ('2010-01-12 11:20:40', '2010-01-12 10:45:00')]
+
+
+@pytest.mark.parametrize("large, short, expected",[(large, short, large_short), (large, differentday, empty), (large_3_intervals, short_3_intervals, large_3_intervals_short_3_intervals), (large, short_boundary_overlap, empty )])
+def test_all_edge_cases(large, short, expected):
+    result = compute_overlap_time(large, short)
+    assert result == expected
+
+'''
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
@@ -34,7 +56,7 @@ def test_boundary_overlap():
     expected = []
     result = compute_overlap_time(large, short)
     assert result == expected
-    
+'''    
 
 def test_backwards_time():
     large = time_range("2010-01-12 12:00:00", "2010-01-12 10:00:00")
