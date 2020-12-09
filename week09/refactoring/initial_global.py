0886bb8cbf22d9ba8aeee549b61d0b3c91a0ed50
Sophie <sophiedarrouxmartin@gmail.com>
2020-12-09 19:22:56 +0000

    Removes global variables: answers #166

--- week09/refactoring/initial_global.py
+++ week09/refactoring/initial_global.py
@@ -1,16 +1,16 @@
-def average_age():
+def average_age(group):
     """Compute the average age of the group's members."""
     all_ages = [person["age"] for person in group.values()]
     return sum(all_ages) / len(group)
 
 
-def forget(person1, person2):
+def forget(person1, person2, group):
     """Remove the connection between two people."""
     group[person1]["relations"].pop(person2, None)
     group[person2]["relations"].pop(person1, None)
 
 
-def add_person(name, age, job, relations):
+def add_person(name, age, job, relations, group):
     """Add a new person with the given characteristics to the group."""
     new_person = {
         "age": age,
@@ -20,7 +20,8 @@ def add_person(name, age, job, relations):
     group[name] = new_person
 
 
-group = {
+if __name__ == "__main__":
+    group = {
     "Jill": {
         "age": 26,
         "job": "biologist",
@@ -45,17 +46,18 @@ group = {
     }
 }
 
-nash_relations = {
-    "John": "cousin",
-    "Zalika": "landlord"
-}
+    nash_relations = {
+        "John": "cousin",
+        "Zalika": "landlord"
+    }
 
-add_person("Nash", 34, "chef", nash_relations)
 
-forget("Nash", "John")
+    add_person("Nash", 34, "chef", nash_relations, group)
 
-if __name__ == "__main__":
+    forget("Nash", "John", group)
+
+
     assert len(group) == 4, "Group should have 4 members"
-    assert average_age() == 28.75, "Average age of the group is incorrect!"
+    assert average_age(group) == 28.75, "Average age of the group is incorrect!"
     assert len(group["Nash"]["relations"]) == 1, "Nash should only have one relation"
     print("All assertions have passed!")
